694aebb73de5f130d3deb203f9a648d79e2421d2
Viktor Gunba <gunba.viktor@gmail.com>
2024-12-16 21:10:46 +0300

    Improving the Application URL.

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -6,13 +6,13 @@ from .views import (CommentViewSet, GroupViewSet, PostViewSet,
                     UserRegistrationView, UserViewSet)
 
 router = DefaultRouter()
-router.register(r'posts', PostViewSet)
-router.register(r'users', UserViewSet)
-router.register(r'groups', GroupViewSet)
-router.register(r'posts\/\d+\/comments', CommentViewSet)
+router.register('posts', PostViewSet)
+router.register('users', UserViewSet)
+router.register('groups', GroupViewSet)
+router.register(r'posts/\d+/comments', CommentViewSet)
 
 urlpatterns = [
-    path('', include(router.urls)),
-    path('api-token-auth/', views.obtain_auth_token),
+    path('api/v1/', include(router.urls)),
+    path('api/v1/api-token-auth/', views.obtain_auth_token),
     path('register/', UserRegistrationView.as_view()),
 ]
